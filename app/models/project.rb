a4b03ec587a6152dc066de96035c53fcb8c7f2bd
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-19 19:22:36 +0530

    allow avatar (image) on project record

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :category
   belongs_to :creator
+
+  # could use bin/rails g model project avatar:attachment, when model is not generated
+  has_one_attached :avatar 
 end
