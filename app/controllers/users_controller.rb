faef9f885b47d875196c152ce23f300df2ff4bec
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-19 19:59:18 +0530

    render user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
     def new
         render inertia: 'users/SignUp'
     end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: { message: "TODO" }
+        else
+            render json: { message: "TODO" }
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- app/javascript/pages/users/SignUp.tsx
+++ app/javascript/pages/users/SignUp.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import type { FC } from 'react'
+import { useForm } from "react-hook-form";
 
 interface Props {
 }
 
 const SignUp: FC<Props> = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+  
   return (
     <div>
-        signup form
+        <h1>signup form</h1>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register("username", { required: true })} />
+            {errors.username && <span>This field is required</span>}
+
+            <input type="password" {...register("password", { required: true })} />
+            {errors.password && <span>This field is required</span>}
+
+            <input type="submit" />
+            </form>
+
+        </div>
     </div>
   )
 }
