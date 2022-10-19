676ebc6e0d0ccd4fa77b0dd3504728570965268c
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-19 18:58:16 +0530

    bin/rails g model reward amount:decimal title:string description:text project:belongs_to

---
+++ db/migrate/20221019132752_create_rewards.rb
@@ -0,0 +1,12 @@
+class CreateRewards < ActiveRecord::Migration[7.0]
+  def change
+    create_table :rewards do |t|
+      t.decimal :amount
+      t.string :title
+      t.text :description
+      t.belongs_to :project, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
