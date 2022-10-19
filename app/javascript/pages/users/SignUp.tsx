d0c503b89f3aea14e0253eb6c8c88232f1c713d9
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-19 23:45:45 +0530

    react form validations

--- app/javascript/pages/users/SignUp.tsx
+++ app/javascript/pages/users/SignUp.tsx
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { FC } from "react";
 import { useForm } from "react-hook-form";
 import { Inertia } from "@inertiajs/inertia";
 
-interface Props {}
+interface Props {
+  errors: Array<Record<string, string>>
+}
 
-const SignUp: FC<Props> = () => {
+const SignUp: FC<Props> = ({errors}) => {
   const {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
-  } = useForm();
+    formState: { errors: formErrors },
+    setError
+  } = useForm<{username: string, password: string}>();
   const onSubmit = (data) => {
-    Inertia.post("/users", data);
+    Inertia.post("/users", {user: {...data, password_confirmation: data.password}});
   };
+
+  useEffect(() => {
+    if(errors) {
+      if (Object.keys(errors).length) {
+        
+        errors.forEach((err, i) => {
+          const key = Object.keys(err).toString()
+          setError(key as 'username' | 'password',  {type: "manual", message: err[key]})
+      })
+      }
+    }
+  }, [errors])
+
 
   return (
     <div>
       <h1>signup form</h1>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("username", { required: true })} />
-          {errors.username && <span>This field is required</span>}
+          <input {...register("username", { required: "provide user name" })} />
+          {formErrors.username && <span>{formErrors?.username?.message}</span>}
 
           <input
             type="password"
-            {...register("password", { required: true })}
+            {...register("password", { required: "provide password" })}
           />
-          {errors.password && <span>This field is required</span>}
+          {formErrors.password && <span>{formErrors?.password?.message}</span>}
 
           <input type="submit" />
         </form>
