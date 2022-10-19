ab4bf8bb4245885216432e6ec5436db298028668
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-19 23:45:33 +0530

    install bcrypt and configure has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    include ActiveModel::SecurePassword
+    has_secure_password
+    has_secure_password :recovery_password, validations: false
+
+    validates :username, presence: true, uniqueness: true
 end
