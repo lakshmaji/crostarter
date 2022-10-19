0d74e11266efbc02be7559160d3d9f59b4143d70
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-10-19 23:46:24 +0530

    first error from model errors and retain in same page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,20 @@ class UsersController < ApplicationController
     def create
         user = User.new(user_params)
         if user.save
+            flash[:success] = "Account created, proceed to login ...."
             redirect_to root_path
         else
-            render json: { message: "TODO" }
+
+            error_fields = user.errors.attribute_names
+            errors = error_fields.map { |attribute| { attribute => user.errors.where(attribute).first.full_message }   }.to_a
+
+            redirect_to new_user_path, inertia: { errors: errors }
         end
     end
 
     private
+
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :password_confirmation)
     end
 end
