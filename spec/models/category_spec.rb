02b614c2b2e25de366d70798d9eb937bb7458520
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-11-05 22:03:18 +0530

    test: :test_tube: category should throw error when no name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+    subject { described_class.new }
     it 'it valid with valid attributes' do
-      expect(Category.new).to be_valid
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid with no name' do
+      expect(subject).to_not be_valid
     end
 end
