9333b77733821de8b767f04961bdd3e5e06999a1
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-11-05 22:51:27 +0530

    fix: :recycle: reference creators using User model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :category
-  belongs_to :creator
+  belongs_to :creator, class_name: 'User'
 
   # could use bin/rails g model project avatar:attachment, when model is not generated
   has_one_attached :avatar 
