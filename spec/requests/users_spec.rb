b8e72679e5750c8d761a54c72e33082d4596e937
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-11-06 12:42:40 +0530

    test: :white_check_mark: user field validations during signup

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -41,8 +41,68 @@ RSpec.describe "Users", type: :request do
           post users_path, params: {
             user: valid_attributes
           }
-          expect(response).to redirect_to(root_path)
+          expect(User.count).to eq(1)
       end
+    end
+
+    context 'with invalid parameters' do
+      let(:valid_attributes) do
+        {
+          username: "lakshmaji",
+          password: "donottellanyone",
+          password_confirmation: "donottellanyone",
+        }
+      end
+
+      it 'returns 302 status code' do
+        valid_attributes[:password] = nil
+        post users_path, params: {
+          user: valid_attributes
+        }
+        expect(response.status).to eq 302
+      end
+
+      it 'redirects to users/new on missing password' do
+          valid_attributes[:password] = nil
+          post users_path, params: {
+            user: valid_attributes
+          }
+          expect(response).to redirect_to(new_user_path)
+      end
+
+      it 'has no new user record with missing valid attributes' do
+          valid_attributes[:password] = nil
+          post users_path, params: {
+            user: valid_attributes
+          }
+          expect(User.count).to eq(0)
+      end
+
+      it 'returns errors when password is missing' do
+        valid_attributes[:password] = nil
+        post users_path, params: {
+          user: valid_attributes
+        }
+        expect(session[:inertia_errors]).to include({ :password => 'Password can\'t be blank' })
+      end
+
+
+      it 'returns errors when username is missing' do
+        valid_attributes[:username] = nil
+        post users_path, params: {
+          user: valid_attributes
+        }
+        expect(session[:inertia_errors]).to include({ :username => 'Username can\'t be blank' })
+      end
+
+      it 'returns errors when password confirmation doesnot match password field' do
+        valid_attributes[:password_confirmation] = "456"
+        post users_path, params: {
+          user: valid_attributes
+        }
+        expect(session[:inertia_errors]).to include({ :password_confirmation => 'Password confirmation doesn\'t match Password' })
+      end
+
     end
 
     # it 'render correct react component' do 
