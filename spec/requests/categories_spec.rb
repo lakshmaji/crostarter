b8f6af998094a65c1f281266a08fd88d408033a7
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-11-06 12:06:49 +0530

    test: :white_check_mark: inertia categories spec

--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -1,10 +1,26 @@
 require 'rails_helper'
+require 'inertia_rails/rspec'
 
 RSpec.describe CategoriesController, type: :request do
-    describe 'GET /index' do
+    describe 'GET /index', inertia: true do
         it 'returns the categories page' do
             get categories_path
             expect(response).to have_http_status(200)
+        end
+
+        it 'render correct react component' do 
+            get categories_path
+            expect_inertia.to render_component 'categories/Categories'
+        end
+
+        it 'it has a component name' do 
+            get categories_path
+            expect(inertia.component).to eq 'categories/Categories'
+        end
+
+        it 'it has a component name' do 
+            get categories_path
+            expect_inertia.to include_props({:categories => []})
         end
     end
 end
