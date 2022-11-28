553f9c1fe9ce12fe2d25d48fe48ac9893b3f2fa1
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-11-29 00:59:07 +0530

    feat: signup ui improvements

--- app/javascript/pages/users/SignUp.tsx
+++ app/javascript/pages/users/SignUp.tsx
@@ -2,56 +2,96 @@ import React, { useEffect } from "react";
 import type { FC } from "react";
 import { useForm } from "react-hook-form";
 import { Inertia } from "@inertiajs/inertia";
-import { Link } from '@inertiajs/inertia-react'
+import { Link } from "@inertiajs/inertia-react";
+import styles from "./signin.module.scss";
+import { classNames } from "../../utils/styles";
 
 interface Props {
-  errors: Array<Record<string, string>>
+  errors: Array<Record<string, string>>;
 }
 
-const SignUp: FC<Props> = ({errors}) => {
+const SignUp: FC<Props> = ({ errors }) => {
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors: formErrors },
-    setError
-  } = useForm<{username: string, password: string}>();
+    setError,
+  } = useForm<{ username: string; password: string }>();
   const onSubmit = (data) => {
-    Inertia.post("/users", {user: {...data, password_confirmation: data.password}});
+    Inertia.post("/users", {
+      user: { ...data, password_confirmation: data.password },
+    });
   };
 
   useEffect(() => {
-    if(errors) {
+    if (errors) {
       if (Object.keys(errors).length) {
-        
         errors.forEach((err, i) => {
-          const key = Object.keys(err).toString()
-          setError(key as 'username' | 'password',  {type: "manual", message: err[key]})
-      })
+          const key = Object.keys(err).toString();
+          setError(key as "username" | "password", {
+            type: "manual",
+            message: err[key],
+          });
+        });
       }
     }
-  }, [errors])
-
+  }, [errors]);
 
   return (
-    <div>
-      <h1>signup form</h1>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("username", { required: "provide user name" })} />
-          {formErrors.username && <span>{formErrors?.username?.message}</span>}
+    <section className={styles.section}>
+      <div className={styles.container} id="container">
+        <div
+          className={classNames(styles.form_container, styles.log_in_container)}
+        >
+          {JSON.stringify(errors)}
+          <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+            <h1>Create new account</h1>
+            <div className={styles.social_container}>
+              <a href="#" className="social">
+                <i className="fa fa-facebook fa-2x"></i>
+              </a>
+              <a href="#" className="social">
+                <i className="fab fa fa-twitter fa-2x"></i>
+              </a>
+            </div>
+            <span>or use your account</span>
+            <input
+              type="text"
+              placeholder="user name"
+              {...register("username", { required: "provide user name" })}
+            />
+            {formErrors.username && (
+              <span>{formErrors?.username?.message}</span>
+            )}
+            <input
+              type="password"
+              placeholder="Password"
+              {...register("password", { required: "provide password" })}
+            />
+            {formErrors.password && (
+              <span>{formErrors?.password?.message}</span>
+            )}
+            <Link href="/sessions/new">login</Link>
 
-          <input
-            type="password"
-            {...register("password", { required: "provide password" })}
-          />
-          {formErrors.password && <span>{formErrors?.password?.message}</span>}
-
-          <input type="submit" />
-        </form>
-        <Link href="/sessions/new" as="button" type="button">login</Link>
+            <button>Sign Up</button>
+          </form>
+        </div>
+        <div className={styles.overlay_container}>
+          <div className={styles.overlay}>
+            <div
+              className={classNames(styles.overlay_panel, styles.overlay_right)}
+            >
+              <h1>HTML CSS Sign up Form</h1>
+              <p>
+                This login form is created using pure HTML and CSS. For social
+                icons, FontAwesome is used.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
