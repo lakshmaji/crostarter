332db67e1e41d78d05879959972aa958bd760dc5
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-11-29 18:45:02 +0530

    chore: :art: Replace deprecated functions
    add type defs

--- app/javascript/components/Application.tsx
+++ app/javascript/components/Application.tsx
@@ -1,28 +1,44 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { createInertiaApp } from "@inertiajs/inertia-react";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp, InertiaAppOptionsForCSR } from '@inertiajs/inertia-react';
 import Layout from './Layout';
 import axios from 'axios';
+import { Props } from '../inertia';
 
-const pages = import.meta.glob('../pages/**/*.tsx')
+const pages = import.meta.glob('../pages/**/*.tsx');
 
 const app = () => {
-  const csrfToken = document.querySelector('meta[name=csrf-token]')?.content;
+  const csrfToken = document.querySelector<HTMLMetaElement>('meta[name=csrf-token]')?.content;
   axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
 
-  return createInertiaApp({
-  resolve: async (name) => {
-    const page = (await pages[`../pages/${name}.tsx`]()).default;
-    page.layout = page.layout || Layout
+  return createInertiaApp<InertiaAppOptionsForCSR<Props>>({
+    resolve: async (name) => {
+      // const page = (await pages[`../pages/${name}.tsx`]()).default;
+      // page.layout = page.layout || Layout
+      // return page
 
-    return page
-    // return import(`./${name}`);
-  },
-  setup({ el, App, props }) {
-    const container = document.getElementById(el.id);
-    const root = createRoot(container!);
-    root.render(<App {...props} />);
-  },
-});
-}
-export default app
+      const module = await pages[`../pages/${name}.tsx`]();
+      const page = (module as never as { default: { layout: React.ReactFragment } }).default;
+      page.layout = page.layout || Layout;
+      return page;
+
+      // return pages[`../pages/${name}.tsx`]().then((page: any) => {
+      //   page.default.layout = page.default.layout || Layout;
+      //   return page
+      // });
+
+      // return import(`../pages/${name}.tsx`).then((module) => {
+      //   const page = module.default
+
+      //   page.layout = Layout
+      //   return page
+      // })
+    },
+    setup({ el, App, props }) {
+      const container = document.getElementById(el.id);
+      const root = createRoot(container!);
+      root.render(<App {...props} />);
+    },
+  });
+};
+export default app;
--- app/javascript/ssr/ssr.tsx
+++ app/javascript/ssr/ssr.tsx
@@ -1,21 +1,50 @@
-import ReactDOMServer from 'react-dom/server'
-import { createInertiaApp } from '@inertiajs/inertia-react'
-import cjsCreateServer from '@inertiajs/server'
-import Layout from '../components/Layout'
+import ReactDOMServer from 'react-dom/server';
+import {
+  createInertiaApp,
+  InertiaAppOptionsForCSR,
+  ReactComponent,
+  ReactInstance,
+} from '@inertiajs/inertia-react';
+import type { Page, PageProps } from '@inertiajs/core';
+import cjsCreateServer from '@inertiajs/server';
+import Layout from '../components/Layout';
+import { ComponentType } from 'react';
+import { Props } from '../inertia';
 
-const pages = import.meta.globEagerDefault('../pages/**/*.tsx')
+const pages = import.meta.glob('../pages/**/*.tsx', { eager: true });
 
 // Unwrap the CJS module in @inertiajs/server.
-const createServer = typeof cjsCreateServer === 'function' ? cjsCreateServer : cjsCreateServer.default
+const createServer =
+  typeof cjsCreateServer === 'function' ? cjsCreateServer : cjsCreateServer['default'];
 
-createServer((page) => createInertiaApp({
-  page,
-  render: ReactDOMServer.renderToString,
-  resolve: name => {
-    const page = pages[`../pages/${name}.tsx`];
-    page.layout = page.layout || Layout
+createServer((page: Page) =>
+  createInertiaApp<InertiaAppOptionsForCSR<Props>>({
+    page,
+    render: ReactDOMServer.renderToString,
+    resolve: async (name: string) => {
+      // const page = (await pages[`../pages/${name}.tsx`]()).default;
+      // page.layout = page.layout || Layout
+      // return page
 
-    return page
-  },
-  setup: ({ App, props }) => <App {...props} />,
-}))
+      // const page = pages[`../pages/${name}.tsx`];
+      // page.layout = page.layout || Layout
+      // return page
+
+      const page = ((await pages[`../pages/${name}.tsx`]) as { default: { layout: ComponentType } })
+        .default;
+      page.layout = page.layout || Layout;
+      return page;
+
+      // const module = (await pages[`../pages/${name}.tsx`]) as { default: { layout: ComponentType } };
+      // const page = module.default
+      // if (!page) {
+      //   throw Error('Page not found');
+      // }
+      // page.layout = page.layout || Layout;
+
+      // // return Object.assign({ layout: Layout }, page);
+      // return page;
+    },
+    setup: ({ App, props }) => <App {...props} />,
+  }),
+);
