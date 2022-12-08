5bd1d64e7d49cf3339c838a5d7dcf0f1b31b00e4
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-08 21:16:05 +0530

    refactor: :recycle: rm explicit csrf token handling
    Remove csrf token logic of reading from meta

--- app/javascript/components/Application.tsx
+++ app/javascript/components/Application.tsx
@@ -2,15 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp, InertiaAppOptionsForCSR } from '@inertiajs/inertia-react';
 import Layout from './Layout';
-import axios from 'axios';
 import { Props } from '../inertia';
 
 const pages = import.meta.glob('../pages/**/*.tsx');
 
 const app = () => {
-  const csrfToken = document.querySelector<HTMLMetaElement>('meta[name=csrf-token]')?.content;
-  axios.defaults.headers.common['X-CSRF-Token'] = csrfToken;
-
   return createInertiaApp<InertiaAppOptionsForCSR<Props>>({
     resolve: async (name) => {
       const module = await pages[`../pages/${name}.tsx`]();
