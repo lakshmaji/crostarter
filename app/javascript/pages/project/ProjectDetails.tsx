8c47f7818799aef0ff4a19661b83912faf173c45
Lakshmaji Mutyala <lakshmaji@users.noreply.github.com>
2022-12-21 22:42:11 +0530

    chore: refactor UI styles

--- app/javascript/pages/project/ProjectDetails.tsx
+++ app/javascript/pages/project/ProjectDetails.tsx
@@ -23,118 +23,122 @@ const ProjectDetails: FC<Props> = ({ project }) => {
           content={`Crostarter is a blackhole crowd funding application, where you can raise funds, invest in projects. This is a test application. View ${project.title}`}
         />
       </AppHead>
-      <section>
-        <div className={classNames(styles.ft_single, styles.bg_white)}>
-          <div className={styles.container}>
-            <div className={styles.yd_flex}>
-              <div className={styles.yd_flex_1}>
-                <div className={styles.f_image}>
-                  <img
-                    className={styles.image}
-                    src='http://ydirection.com/Fold/assets/images/dead.gif'
-                    alt='Feature'
-                  />
-                </div>
+      <section className={classNames(styles.section, styles.bg_white)}>
+        <div className={styles.container}>
+          <div className={styles.yd_flex}>
+            <div className={styles.yd_flex_1}>
+              <div className={styles.f_image}>
+                <img
+                  className={styles.image}
+                  src='http://ydirection.com/Fold/assets/images/dead.gif'
+                  alt='Feature'
+                />
               </div>
-              <div className={styles.yd_flex_2}>
-                <h3 className={styles.subtitle}>{project.category?.name}</h3>
-                <h2 className={styles.title}>{project.title}</h2>
-                <p className={styles.description}>{project.description}</p>
-                <button className={styles.contribute}>Contribute</button>
-              </div>
+            </div>
+            <div className={styles.yd_flex_2}>
+              <h3 className={styles.subtitle}>{project.category?.name}</h3>
+              <h2 className={styles.title}>{project.title}</h2>
+              <p className={styles.description}>{project.description}</p>
+              <button className={styles.contribute}>Contribute</button>
             </div>
           </div>
         </div>
       </section>
-      <section>
-        <div className={styles.ft_single}>
-          <div className={styles.container}>
-            <div className={styles.yd_flex}>
-              <div className={styles.yd_flex_1}>
-                <div className={styles.flex_content}>
-                  <h2>Mobile Integration</h2>
-                  <p>
-                    Grow your business with our secure storage, powerful computing, and integrated
-                    analytics at affordable price.
-                  </p>
-                  <InertiaLink className={classNames(styles.btn_action, styles.btn_alt)} href='#'>
-                    Get Started
-                  </InertiaLink>
-                </div>
-              </div>
-              <div className={styles.yd_flex_2}>
-                <div className={styles.flex_main}>
-                  <div className={styles.flex_sub}>
-                    <div className={styles.sub_image}>
-                      <IoPhonePortraitOutline size={45} color='#fff' />
-                    </div>
-                    <div className={styles.sub_text}>
-                      <h4>Go Mobile</h4>
-                      <p className='d-none d-md-block'>
-                        Some lorem ipsum text filler lorem ipsum text
-                      </p>
-                    </div>
-                  </div>
-                  <div className={styles.flex_sub}>
-                    <div className={styles.sub_image}>
-                      <IoDesktopSharp size={45} color='#fff' />
-                    </div>
-                    <div className={styles.sub_text}>
-                      <h4>Video</h4>
-                      <p className='d-none d-md-block'>
-                        Some lorem ipsum text filler lorem ipsum text
-                      </p>
-                    </div>
-                  </div>
-                  <div className={styles.flex_sub}>
-                    <div className={styles.sub_image}>
-                      <IoLogoUsd size={45} color='#fff' />
-                    </div>
-                    <div className={styles.sub_text}>
-                      <h4>Asset Track</h4>
-                      <p className='d-none d-md-block'>
-                        Some lorem ipsum text filler lorem ipsum text
-                      </p>
-                    </div>
-                  </div>
-                  <div className={styles.flex_sub}>
-                    <div className={styles.sub_image}>
-                      <IoChatbubblesOutline size={45} color='#fff' />
-                    </div>
-                    <div className={styles.sub_text}>
-                      <h4>Profiling</h4>
-                      <p className='d-none d-md-block'>
-                        Some lorem ipsum text filler lorem ipsum text
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section>
-        <div className={classNames(styles.ft_single, styles.bg_white)}>
-          <div className={styles.container}>
-            <div className={styles.yd_flex}>
-              <div className={styles.yd_flex_1}>
-                <h3 className={styles.subtitle}>{project.category?.name}</h3>
-                <h2 className={styles.title}>title comes here</h2>
-                <p className={styles.description}>
-                  {project.details}
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit, sit. At
-                  provident veniam maiores, iusto odio minus inventore saepe nesciunt unde
-                  aspernatur fugiat, voluptates in obcaecati magni perferendis quisquam tempore.
+      <section className={classNames(styles.section, styles.bg_blue)}>
+        <div className={styles.container}>
+          <div className={styles.yd_flex}>
+            <div className={styles.yd_flex_1}>
+              <div className={styles.flex_content}>
+                <h2>Mobile Integration</h2>
+                <p>
+                  Grow your business with our secure storage, powerful computing, and integrated
+                  analytics at affordable price.
                 </p>
-                <button className={styles.contribute}>Contribute</button>
-              </div>
-              <div className={styles.yd_flex_2}>
-                <Suspense fallback={<div>Loading...</div>}>
-                  <ProjectStats />
-                </Suspense>
+                <InertiaLink className={classNames(styles.btn_action, styles.btn_alt)} href='#'>
+                  Get Started
+                </InertiaLink>
               </div>
             </div>
+            <div className={styles.yd_flex_2}>
+              <div className={styles.flex_main}>
+                <div className={styles.flex_sub}>
+                  <div className={styles.sub_image}>
+                    <IoPhonePortraitOutline size={45} color='#fff' />
+                  </div>
+                  <div className={styles.sub_text}>
+                    <h4>Go Mobile</h4>
+                    <p className='d-none d-md-block'>
+                      Some lorem ipsum text filler lorem ipsum text
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.flex_sub}>
+                  <div className={styles.sub_image}>
+                    <IoDesktopSharp size={45} color='#fff' />
+                  </div>
+                  <div className={styles.sub_text}>
+                    <h4>Video</h4>
+                    <p className='d-none d-md-block'>
+                      Some lorem ipsum text filler lorem ipsum text
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.flex_sub}>
+                  <div className={styles.sub_image}>
+                    <IoLogoUsd size={45} color='#fff' />
+                  </div>
+                  <div className={styles.sub_text}>
+                    <h4>Asset Track</h4>
+                    <p className='d-none d-md-block'>
+                      Some lorem ipsum text filler lorem ipsum text
+                    </p>
+                  </div>
+                </div>
+                <div className={styles.flex_sub}>
+                  <div className={styles.sub_image}>
+                    <IoChatbubblesOutline size={45} color='#fff' />
+                  </div>
+                  <div className={styles.sub_text}>
+                    <h4>Profiling</h4>
+                    <p className='d-none d-md-block'>
+                      Some lorem ipsum text filler lorem ipsum text
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={classNames(styles.section, styles.bg_white)}>
+        <div className={styles.container}>
+          <div className={styles.yd_flex}>
+            <div className={styles.yd_flex_1}>
+              <h3 className={styles.subtitle}>{project.category?.name}</h3>
+              <h2 className={styles.title}>{project.title}</h2>
+              <p className={styles.description}>{project.details}</p>
+              {/* <button className={styles.contribute}>Contribute</button> */}
+              <p>The contributions graph</p>
+            </div>
+            <div className={styles.yd_flex_2}>
+              <Suspense fallback={<div>Loading...</div>}>
+                <ProjectStats />
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className={classNames(styles.section, styles.bg_white)}>
+        <div className={styles.container}>
+          <div className={styles.yd_flex}>
+            <div className={styles.yd_flex_1}>
+              <h2 className={styles.title}>Reward options</h2>
+              <h3 className={styles.subtitle}>{project.category?.name}</h3>
+              <p className={styles.description}>{project.details}</p>
+              {/* <button className={styles.contribute}>Contribute</button> */}
+              <p>The contributions graph</p>
+            </div>
+            <div className={styles.yd_flex_2}>form with prefilled data with chosen reward</div>
           </div>
         </div>
       </section>
