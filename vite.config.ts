a6eb3f0e79ba33d067c38939fe5cffce5593de09
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-27 01:30:37 +0530

    feat: :zap: vendor chunks
    remove turbo js files

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,29 @@ import { defineConfig } from 'vite';
 import RubyPlugin from 'vite-plugin-ruby';
 import ReactPlugin from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { dependencies } from './package.json';
+
+function renderChunks(deps: Record<string, string>) {
+  const chunks: Record<string, any> = {};
+  Object.keys(deps).forEach((key) => {
+    if (['react', 'react-select', 'recharts', 'react-dom'].includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
 
 export default defineConfig({
-  build: { sourcemap: false },
+  build: {
+    sourcemap: false,
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-select', 'recharts', 'react-dom'],
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
   plugins: [RubyPlugin(), ReactPlugin(), tsconfigPaths()],
 });
