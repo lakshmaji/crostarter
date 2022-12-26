3f9c335560aeb0db8445163711fe54fea3a4e5cd
Lakshmaji Mutyala <lakshmaji@users.noreply.github.com>
2022-12-27 01:42:04 +0530

    feat: perf ⚡  (#68)
    * chore: :heavy_minus_sign: rm unused
    
    * feat: :zap: vendor chunks
    
    remove turbo js files
    
    * feat: :zap: use splitVendorChunkPlugin instead of manual chunks

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import RubyPlugin from 'vite-plugin-ruby';
 import ReactPlugin from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { splitVendorChunkPlugin } from 'vite';
 
 export default defineConfig({
-  build: { sourcemap: false },
-  plugins: [RubyPlugin(), ReactPlugin(), tsconfigPaths()],
+  build: {
+    sourcemap: false,
+  },
+  plugins: [RubyPlugin(), ReactPlugin(), tsconfigPaths(), splitVendorChunkPlugin()],
 });
