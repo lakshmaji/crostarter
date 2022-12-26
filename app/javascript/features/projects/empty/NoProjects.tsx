dc80228c2d81addf1394a937ec12391d30400f4e
Lakshmaji Mutyala <lakshmaji@users.noreply.github.com>
2022-12-26 22:50:49 +0530

    feat: :sparkles: empty projects (#56)

---
+++ app/javascript/features/projects/empty/NoProjects.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './no-projects.module.scss';
+
+const NoProjects = () => {
+  return <div className={styles.container}>No Projects</div>;
+};
+
+export default NoProjects;
