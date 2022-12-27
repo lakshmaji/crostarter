2d0620b37bf86ded3f54aa16ad1e998562ed05ae
Lakshmaji Mutyala <lakshmaji@users.noreply.github.com>
2022-12-27 18:07:52 +0530

    feat: :sparkles: rack-attack for DDoS (#53)
    * feat: :sparkles: rack-attack for DDoS
    
    * feat: :sparkles: customize 429 response

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# Allow max 5 requests
+# In period of 60 seconds
+Rack::Attack.throttle('requests by ip', limit: 5, period: 60.seconds, &:ip)
+
+# Throttle login attempts for a given username(email) parameter to 10 reqs/minute
+# Return the *normalized* username as a discriminator on POST /login requests
+Rack::Attack.throttle('limit logins per username', limit: 10, period: 60.seconds) do |req|
+  if req.path == '/session/new' && req.post?
+    # Normalize the username, using the same logic as your authentication process, to
+    # protect against rate limit bypasses.
+    req.params['username'].to_s.downcase.gsub(/\s+/, '')
+  end
+end
+
+Rack::Attack.throttled_responder = lambda do |_request|
+  # Using 503 because it may make attacker think that they have successfully
+  # DOSed the site. Rack::Attack returns 429 for throttling by default
+  [503, {}, ["This application run on free shared instance, show some love 🥹 \n"]]
+end
