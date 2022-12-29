ab2e6018697c83909447b288876ef9e08475a73e
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-29 19:18:21 +0530

    test: :white_check_mark: signup and login e2e specs

---
+++ e2e/auth.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('signup', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'New? Create account' }).click();
+  await page.getByPlaceholder('user name').click();
+  await page.getByPlaceholder('user name').fill('demo@account.com');
+  await page.getByPlaceholder('user name').press('Tab');
+  await page.getByPlaceholder('Password').fill('secret');
+  await page.getByRole('button', { name: 'Sign Up' }).click();
+});
+
+test('login', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByPlaceholder('user name').click();
+  await page.getByPlaceholder('user name').fill('demo@account.com');
+  await page.getByPlaceholder('user name').press('Tab');
+  await page.getByPlaceholder('Password').fill('secret');
+  await page.getByRole('button', { name: 'Log In' }).click();
+
+  const username = await page.getByTestId('username');
+  await expect(username).toHaveText('demo@account.com');
+});
