02e3cd811cb93799d5acc0ab8135edc11ac81f2e
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-31 00:56:08 +0530

    feat: :sparkles: mailer preview

--- test/mailers/previews/welcome_user_mailer_preview.rb
+++ test/mailers/previews/welcome_user_mailer_preview.rb
@@ -1,4 +1,6 @@
-# Preview all emails at http://localhost:3000/rails/mailers/welcome_user_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/welcome_user_mailer/welcome_email
 class WelcomeUserMailerPreview < ActionMailer::Preview
-
+  def welcome_email
+    WelcomeUserMailer.with(user: User.first).welcome_email
+  end
 end
