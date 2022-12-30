3849becc1f445bc084255ae73edd5af197ca0f26
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-30 13:25:35 +0530

    feat: :sparkles: add seed generator usage template

--- lib/generators/seed/seed_generator.rb
+++ lib/generators/seed/seed_generator.rb
@@ -1,4 +1,5 @@
 class SeedGenerator < Rails::Generators::Base
+  source_root File.expand_path('templates', __dir__)
   def create_seed_file
     create_file('db/seeds/dummy.rb', '# Add initiali contst here')
   end
