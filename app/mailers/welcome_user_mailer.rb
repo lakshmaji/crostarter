80eb779f90efc59cdbc9cf8535965ae273ea27e7
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-31 00:47:50 +0530

    feat: :sparkles: create welcome user mailer
    bin/rails g mailer welcome_user

---
+++ app/mailers/welcome_user_mailer.rb
@@ -0,0 +1,2 @@
+class WelcomeUserMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/welcome_user_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/welcome_user_mailer
+class WelcomeUserMailerPreview < ActionMailer::Preview
+
+end
