076a62681ce5c9fe5e457c47bcf764a601e1cc19
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-30 16:24:49 +0530

    feat: :sparkles: create seeds with timestamp

--- lib/generators/seed/seed_generator.rb
+++ lib/generators/seed/seed_generator.rb
@@ -2,7 +2,39 @@
 
 class SeedGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
-  def copy_seed_file
-    copy_file('seed.rb', "db/seeds/#{file_name}.rb")
+  def generate_seed
+    generate_seed_file
+  end
+
+  private
+
+  def generate_seed_file
+    destination_root = db_seed_path
+    destination = "#{name}_seed.rb"
+    destination = File.expand_path(destination, destination_root)
+    seed_dir = File.dirname(destination)
+    seed_number = next_seed_number(seed_dir)
+    dir, base = File.split(destination)
+    numbered_destination = File.join(dir, [seed_number, base].join('_'))
+    template('seed.rb', numbered_destination)
+  end
+
+  def next_seed_number(dirname)
+    number = current_seed_number(dirname) + 1
+    [Time.now.utc.strftime('%Y%m%d%H%M%S'), '%.14d' % number].max
+  end
+
+  def seed_lookup_at(dirname)
+    Dir.glob("#{dirname}/[0-9]*_*.rb")
+  end
+
+  def current_seed_number(dirname)
+    seed_lookup_at(dirname).collect do |file|
+      File.basename(file).split('_').first.to_i
+    end.max.to_i
+  end
+
+  def db_seed_path
+    'db/seeds'
   end
 end
