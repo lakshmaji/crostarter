ab13865b9ca6f733e880ec8044c29a6f6e5fecf9
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-31 03:47:29 +0530

    feat: :fire: rm password digest from gql

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,7 +4,6 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :username, String
-    field :password_digest, String
     field :session_token, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
