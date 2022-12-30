bd641c85577848d72628b9cafdf363711dcbccb3
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-30 18:13:37 +0530

    feat: :sparkles: Rack attack, ignore throttle for request
    Allow API requests with CI_API_KEY header
    
    update throttle_key for both dev and prod environments

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,7 +2,7 @@
 
 # Allow max 5 requests
 # In period of 60 seconds
-Rack::Attack.throttle('requests by ip', limit: 5, period: 60.seconds, &:ip) unless Rails.env.development?
+Rack::Attack.throttle('requests by ip', limit: 5, period: 60.seconds, &:ip)
 
 # Throttle login attempts for a given username(email) parameter to 10 reqs/minute
 # Return the *normalized* username as a discriminator on POST /login requests
@@ -18,4 +18,10 @@ Rack::Attack.throttled_responder = lambda do |_request|
   # Using 503 because it may make attacker think that they have successfully
   # DOSed the site. Rack::Attack returns 429 for throttling by default
   [503, {}, ["This application run on free shared instance, show some love 🥹 \n"]]
+end
+
+# Provided that trusted users use an HTTP request header named APIKey
+Rack::Attack.safelist('mark any authenticated access safe') do |request|
+  # Requests are allowed if the return value is truthy
+  request.env['HTTP_CI_API_KEY'] == Rails.application.credentials.dig(:ci, :throttle_key)
 end
