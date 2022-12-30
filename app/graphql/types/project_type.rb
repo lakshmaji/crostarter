7bd7babeefb2c1c33b0a55c4e0dbc467d04eb1b3
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-31 03:49:01 +0530

    feat: :sparkles: gen Project type
    bin/rails g graphql:object Project

---
+++ app/graphql/types/project_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class ProjectType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String
+    field :website, String
+    field :description, String
+    field :end_date, GraphQL::Types::ISO8601Date
+    field :funding_goal, Float
+    field :details, String
+    field :category_id, Integer, null: false
+    field :creator_id, Integer, null: false
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
