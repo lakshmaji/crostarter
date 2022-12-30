3c4c9c33958c286d959736fc79ce0d74cbc0cb91
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-31 00:55:50 +0530

    feat: :sparkles: welcome user mail

--- app/mailers/welcome_user_mailer.rb
+++ app/mailers/welcome_user_mailer.rb
@@ -1,2 +1,7 @@
 class WelcomeUserMailer < ApplicationMailer
+  def welcome_email
+    @user = params[:user]
+    @url  = 'https://crostarter.fly.dev/login'
+    mail(to: @user.username, subject: 'Welcome to Crostarter')
+  end
 end
