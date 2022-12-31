06ec7acb35b21c0f65839c5b348561d1a784f810
Lakshmaji <lakshmaji@users.noreply.github.com>
2022-12-31 16:16:35 +0530

    feat: :sparkles: Projects under a category

--- app/graphql/types/category_type.rb
+++ app/graphql/types/category_type.rb
@@ -8,5 +8,14 @@ module Types
     field :icon_path_name, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :projects, [Types::ProjectType], null: true
+    field :projects_count, Integer, 'No of projects', null: true
+
+    # Typical rails method returning the count
+    # of total projects for each category
+    def projects_count
+      object.projects.size
+      # 10
+    end
   end
 end
